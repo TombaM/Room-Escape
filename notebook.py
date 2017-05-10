4e9fbee8ace972fa0287b23f37f52552e0991101
TombaM <tomicass3@gmail.com>
2017-05-10 15:49:37 +0200

    file name changed

---
+++ notebook.py
@@ -0,0 +1,11 @@
+import pygame
+
+class Notebook(pygame.sprite.Sprite):  
+  def __init__(self, image, location):
+    pygame.sprite.Sprite.__init__(self)
+
+    self.image = pygame.image.load(image)
+    self.image = pygame.transform.scale(self.image, (350, 300))
+
+    self.rect = self.image.get_rect()
+    self.rect.center = location
